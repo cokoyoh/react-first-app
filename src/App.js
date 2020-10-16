957b7c791f006e4b178c2164dc79d7f0c785675f
cokoyoh <charlesokoyoh@gmail.com>
2020-10-16 21:37:10 +0300

    Intro to jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-const title = 'React';
+const getTitle = title => title;
 
 function App() {
   return (
     <div>
-      <h1>Hello {title}!</h1>
+      <h1>Hello {getTitle('React')}</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"/>
     </div>
   );
 }
