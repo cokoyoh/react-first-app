fa2e8a8c5be7359b71b723c69980bba91704c82a
cokoyoh <charlesokoyoh@gmail.com>
2020-11-01 10:02:36 +0300

    Remove generic search component and replace with a resusable InputWithLabel component

--- src/App.js
+++ src/App.js
@@ -52,7 +52,12 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search search={searchTerm} onSearch={handleSearch} />
+      <InputWithLabel
+        id="search"
+        label="Search"
+        value={searchTerm}
+        onInputChange={handleSearch}
+      />
 
       <hr />
 
@@ -61,19 +66,23 @@ const App = () => {
   );
 };
 
-const Search = ({ search, onSearch }) => [
-  <>
-    <label htmlFor="search">
-      Search: {''}
-      </label>
-    <input
-      id="search"
-      type="text"
-      value={search}
-      onChange={onSearch}
-    />
-  </>
-];
+const InputWithLabel = ({
+  id,
+  label,
+  value,
+  type = "text",
+  onInputChange
+}) => (
+    <>
+      <label htmlFor={id}> {label} </label>
+      <input
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
+    </>
+  );
 
 const List = ({ list }) =>
   list.map(({ objectID, ...item }) =>
